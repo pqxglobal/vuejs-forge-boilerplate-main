932631608da6e40e97a8f29c4374b2115956e19f
Paul Onawola <onawola.ire@gmail.com>
2022-07-13 16:33:41 +0100

    Exercise 2 Commit

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Button as KButton } from "@progress/kendo-vue-buttons";
+import { Popup as KPopup } from "@progress/kendo-vue-popup";
+</script>
+
+<template>
+  <div>
+    <button ref="button" class="mr-5 text-[]"></button>
+    <KPopup> </KPopup>
+  </div>
+</template>
+
+<style scoped></style>
